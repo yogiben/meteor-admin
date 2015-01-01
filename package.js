b3c1dc1f1cd67665722070c2e7dec2acd85cfdd0
Michal Powaga <michalpowaga13@gmail.com>
2015-01-01 23:10:00 +0000

    Update dependencies

--- package.js
+++ package.js
@@ -12,17 +12,17 @@ Package.on_use(function(api){
   api.versionsFrom('METEOR@1.0');
 
   api.use(
-    ['iron:router@1.0.3',
+    ['iron:router@1.0.6',
     'coffeescript',
     'accounts-base',
     'accounts-password',
     'underscore',
-    'aldeed:collection2@2.2.0',
-    'aldeed:autoform@4.0.7',
+    'aldeed:collection2@2.3.0',
+    'aldeed:autoform@4.2.2',
     'aldeed:template-extension@3.1.1',
     'alanning:roles@1.2.13',
-    'raix:handlebar-helpers@0.1.3',
-    'alethes:pages@1.7.1'
+    'raix:handlebar-helpers@0.2.4',
+    'alethes:pages@1.7.2'
     ],
     both);
 
