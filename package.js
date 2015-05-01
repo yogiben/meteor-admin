949ed18d2278874c5d130a7e77028e09b392ff19
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-05-01 08:25:06 +0100

    Bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "yogiben:admin",
   summary: "A complete admin dashboard solution",
-  version: "1.1.0",
+  version: "1.1.1",
   git: "https://github.com/yogiben/meteor-admin"
 });
 
