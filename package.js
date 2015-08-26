3280199d4674f9d2efc50b3b8b8a1bc49f3bd5d3
Carlos Baraza Haro <carlos.baraza@ieee.org>
2015-08-26 23:16:46 +0200

    Fix skinning

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "yogiben:admin",
+  name: "carlosbaraza:admin",
   summary: "A complete admin dashboard solution",
   version: "1.2.2",
   git: "https://github.com/yogiben/meteor-admin"
